6311c54bd594b659a3066ec69c67f1c9baa79949
ujjawal <ujjawals229@gmail.com>
2022-03-30 22:11:48 +0530

    done

--- close.js
+++ close.js
@@ -8,22 +8,3 @@ btnc.addEventListener("click", function () {
   closewindow();
   console.log("clicked");
 });
-
-function closewindow() {
-  window.open("./index.html", "_parent");
-}
-const btn2 = document.querySelector("#btn2");
-
-btn2.addEventListener("click", function () {
-  closewindow();
-  console.log("clicked");
-});
-function closewindow() {
-  window.open("./index.html", "_parent");
-}
-const btn3 = document.querySelector("#btn3");
-
-btn3.addEventListener("click", function () {
-  closewindow();
-  console.log("clicked");
-});
--- script.js
+++ script.js
@@ -75,4 +75,28 @@ const btn3 = document.querySelector("#btn3");
 btn3.addEventListener("click", function () {
   openwindow3();
 });
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openpopup = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closepopup = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const btn_login = document.querySelector("#btn_login");
+btn_login.addEventListener("click", openpopup);
 
+const btn_close = document.querySelector("#btn_close");
+btn_close.addEventListener("click", closepopup);
+
+function writing() {
+  window.open("./writing.html", "_parent");
+}
+const btn_pop = document.querySelector("#btn_pop");
+btn_pop.addEventListener("click", function () {
+
+
+  writing();
+});
